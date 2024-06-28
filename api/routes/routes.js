c5cb7ad3535d03d06510913704d14497cdc92b4f
DevAmade <justin.dores@outlook.fr>
2024-06-28 13:07:49 +0200

    3rd commit

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,32 +2,83 @@ const express = require('express');
 const pool = require('../db/db.js');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const text = "SELECT * FROM users";
-
+router.get('/:id', async (req, res) => {
     try {
-        const result = await pool.query(text);
-        console.log(result);
+        const text = "SELECT * FROM users WHERE id = $1";
+        const values = [req.params.id];
+        const result = await pool.query(text, values);
+
         res.status(200).send(result);
     } catch(err) {
         console.error(err.stack);
     }
 });
 
-router.post('/', async (req, res) => {
-    console.log(req.body);
-    
-    const text = "INSERT INTO users (last_name, first_name, email, birthdate) VALUES ($1, $2, $3, $4)";
-    const values = [
-        req.body.lastName,
-        req.body.firstName,
-        req.body.email,
-        req.body.birthdate,
-    ];
-
+router.get('/', async (req, res) => {
     try {
+        const text = "SELECT * FROM users";
+        const result = await pool.query(text);
+
+        res.status(200).send(result);
+    } catch(err) {
+        console.error(err.stack);
+    }
+});
+
+router.post('/add-data', async (req, res) => {
+    try {
+        req.body.forEach(async user => {
+            const text = "INSERT INTO users (last_name, first_name, email, birthdate) VALUES ($1, $2, $3, $4)";
+            const values = [
+                user.lastName,
+                user.firstName,
+                user.email,
+                user.birthdate,
+            ];
+            await pool.query(text, values);
+        });
+
+        res.sendStatus(200);
+    } catch(err) {
+        console.error(err.stack);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const text = "INSERT INTO users (last_name, first_name, email, birthdate) VALUES ($1, $2, $3, $4)";
+        const values = [
+            req.body.lastName,
+            req.body.firstName,
+            req.body.email,
+            req.body.birthdate,
+        ];
         await pool.query(text, values);
-        res.sendStatus(201);
+
+        res.sendStatus(200);
+    } catch(err) {
+        console.error(err.stack);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const text = "DELETE FROM users WHERE id = $1";
+        const values = [req.params.id];
+        await pool.query(text, values);
+
+        res.sendStatus(200);
+    } catch(err) {
+        console.error(err.stack);
+    }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+        const text = "TRUNCATE users RESTART IDENTITY";
+        await pool.query(text);
+
+        res.sendStatus(200);
     } catch(err) {
         console.error(err.stack);
     }
