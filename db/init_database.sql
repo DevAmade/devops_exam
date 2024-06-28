60474efa977763c0633c3af0301901b451faa78d
DevAmade <justin.dores@outlook.fr>
2024-06-28 13:52:15 +0200

    6th commit

--- db/init_database.sql
+++ db/init_database.sql
@@ -8,10 +8,11 @@ CREATE TABLE users (
     birthdate DATE
 );
 
-INSERT INTO users (last_name, first_name, email, birthdate)
-VALUES ("Vador", "Dark", "vador.dark@black-start.empire", "12/09/1207"),
-       ("D'an", "Gul", "gul.d'an@legion.horde", "12/09/1207"),
-       ("King", "Lich", "arthas.king@citadelle.fleau", "02/08/1490"),
-       ("Kenobi", "Obi-wan", "obi-wan.kenobi@naboo.repu", "15/03/1960"),
-       ("Sparrow", "Jack", "jack.sparrow@pearl.tortuga", "12/09/1207"),
-       ("Jones", "Davy", "davy.jones@flying-dutsh.tortuga", "08/04/1478");
+INSERT INTO users (last_name, first_name, email, birthdate) 
+VALUES 
+('Vador', 'Dark', 'vador.dark@black-start.empire', '1977-06-25'), 
+('Dan', 'Gul', 'gul.dan@legion.horde', '1207-09-12'),
+('King', 'Lich', 'arthas.king@citadelle.fleau', '1490-08-02'),
+('Kenobi', 'Obi-wan', 'obi-wan.kenobi@naboo.repu', '1960-03-15'),
+('Sparrow', 'Jack', 'jack.sparrow@pearl.tortuga', '1367-11-17'),
+('Jones', 'Davy', 'davy.jones@flying-dutsh.tortuga', '1478-04-08');
