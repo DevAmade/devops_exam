c5fc246053dcb04e6a267c5f74ddffa12559dffc
DevAmade <justin.dores@outlook.fr>
2024-06-28 13:41:15 +0200

    5th commit

--- db/init_database.sql
+++ db/init_database.sql
@@ -7,3 +7,11 @@ CREATE TABLE users (
     email VARCHAR(100),
     birthdate DATE
 );
+
+INSERT INTO users (last_name, first_name, email, birthdate)
+VALUES ("Vador", "Dark", "vador.dark@black-start.empire", "12/09/1207"),
+       ("D'an", "Gul", "gul.d'an@legion.horde", "12/09/1207"),
+       ("King", "Lich", "arthas.king@citadelle.fleau", "02/08/1490"),
+       ("Kenobi", "Obi-wan", "obi-wan.kenobi@naboo.repu", "15/03/1960"),
+       ("Sparrow", "Jack", "jack.sparrow@pearl.tortuga", "12/09/1207"),
+       ("Jones", "Davy", "davy.jones@flying-dutsh.tortuga", "08/04/1478");
